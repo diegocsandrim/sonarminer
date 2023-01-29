15a80013d39fadb648f5702ca1995f11b95c71fd
Diego Cardozo Sandrim <diegocsandrim@gmail.com>
2023-01-28 21:14:29 -0300

    Remove unused log msg

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -23,9 +23,6 @@ func (f *CmdFactory) ExecF(command string, params ...interface{}) (string, error
 
 	out, err := cmd.CombinedOutput() //may need to separate stdout and stderr
 	if err != nil {
-		log.Printf("command: %s", cmd.String())
-		log.Printf("output: %s", out)
-		log.Printf("error: %s", err)
 		return string(out), err
 	}
 
